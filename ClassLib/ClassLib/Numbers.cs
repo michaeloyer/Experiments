2f15d2132c77db87ffe2a1c6b4d3dfd286fa157d
Michael Oyer <code@michaeloyer.com>
2019-09-20 21:14:56 -0400

    Initial Commit

---
+++ ClassLib/ClassLib/Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassLib
+{
+    public static class Numbers
+    {
+        private static Random random = new Random();
+        public static int Get()
+        {
+            var x = 10;
+            x++;
+            return x;
+        }
+
+        public static int GetRandom()
+        {
+            return random.Next(1, 11);
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLib;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Numbers.Get());
+            Console.WriteLine(Numbers.GetRandom());
+        }
+    }
+}
